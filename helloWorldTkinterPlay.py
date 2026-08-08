628fbe8aec10069d2da77a1239a46b63a215adad
AbdallahDaradkeh-1 <daradkehabdallah7@gmail.com>
2026-08-08 16:24:06 +0300

    Play a little with tkinter APIs and theirs methods such as tk.geometry and btn.pack, give size for the window, and stretch the button horizontally,vertically, and both. Also, Change the position of the button

--- helloWorldTkinterPlay.py
+++ helloWorldTkinterPlay.py
@@ -5,9 +5,8 @@ def hello():
   print("hello world!")
 
 tk = tk.Tk()
-
+tk.geometry("400x200")
 btn = Button(tk, text = 'click me', command = hello)
 
-btn.pack()
-
-tk.mainloop()
+btn.pack(side=LEFT, fill = X, expand = True)# The X(horizontal), expand = False by default, so we should make it
+tk.mainloop()                 # True To stretch the button horizontally
