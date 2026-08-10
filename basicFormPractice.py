1005914d8885c5da8fd823ea42aa1f7cc1d56f52
AbdallahDaradkeh-1 <daradkehabdallah7@gmail.com>
2026-08-10 14:54:18 +0300

    Configure Main WIndow Background Color, Window Size. Add same background to other widgets because the widgets' default background color is gray, and I want transparent background color so I gave the widgets the same background color as the main Window

--- basicFormPractice.py
+++ basicFormPractice.py
@@ -3,13 +3,17 @@ from tkinter import ttk
 
 root = Tk()
 
+root.title("Pracitce Form")
+root.geometry("500x500")
 
-label1 = Label(root, text="Name")
+root.configure(background="#eaff7b")
+label1 = Label(root, text="Name", background="#eaff7b")
 label1.grid(column=0,row=0) # We made the grid on a seperate line, becuase grid return None, and we don't need the label1 value to be None, we want the label1 to have the widget itself
 
 Entry(root).grid(column=1, row=0)
 
-label2 = Label(root,text="Password")
+root.configure(background="#eaff7b")
+label2 = Label(root,text="Password", background="#eaff7b")
 label2.grid(column=0, row=1)
 Entry(root).grid(column=1,row=1)
 Button(root, text="Quit", command=root.destroy).grid(column=0, row=2)
